84f69aeacfcd852e086dd0b90a362a98e6d2b90a
baylagas <balbino_a@hotmail.com>
2020-10-09 17:54:36 -0600

    init files

---
+++ main_app.py
@@ -0,0 +1,35 @@
+print("Inicio TodoApp: \n")
+
+
+def showAllTasks():
+    pass
+
+
+def createNewTask():
+    pass
+
+
+def modifyTask():
+    pass
+
+
+def markTaskAsDone():
+    pass
+
+
+def deleteTask():
+    pass
+
+
+"""
+while True:
+    print("Menu: \n")
+    print("(0) salir")
+    print("(1) ver todas las tareas")
+    print("(2) crear una nueva tarea")
+    print("(3) modificar una tarea - titulo")
+    print("(4) finalizar una tarea")
+    print("(5) eliminar una tarea \n")
+    option = int(input("opcion: "))
+
+"""
---
+++ todoapp.py
@@ -0,0 +1,24 @@
+class TodoApp:
+    def __init__(self):
+        pass
+
+    def addNewTask(self, taskTitle):
+        pass
+
+    def getAllTasks(self):
+        pass
+
+    def numberOfTasks(self):
+        pass
+
+    def getTaskById(self, taskId):
+        pass
+
+    def updateTask(self, taskId, newTitle):
+        pass
+
+    def updateTaskAsDone(self, taskId):
+        pass
+
+    def deleteTaskById(self, taskId):
+        pass
